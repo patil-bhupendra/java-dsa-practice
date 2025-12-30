2bad761f54851a2fa92d5f78eb6924abcee974c6
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2025-12-30 14:19:45 +0530

    Demonstrate encapsulation using getters and setters in Java

--- oops/OOPS.java
+++ oops/OOPS.java
@@ -1,52 +1,35 @@
 package oops;
 public class OOPS {
     public static void main(String args[]){
-        Pen p1 = new Pen(); //created a pen object called p1
+        Pen p1 = new Pen(); 
         p1.setColor("Blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.setTip(5);
-        System.out.println(p1.tip);
-        // p1.setColor("Yellow");
-        p1.color = "Yellow";
-        System.out.println(p1.color);
-
-        BankAccount myAcc = new BankAccount();
-        myAcc.username = "bhupendraPatil";
-        myAcc.setPassword("abcdefghi");
-        
-        
-    }
-}
-
-
-class BankAccount {
-    public String username;
-    private String password;
-    public void setPassword(String pwd){
-        password = pwd;
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
     }
 }
 
 class Pen {
-    String color;
-    int tip;
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip(){
+        return this.tip;
+    }
 
     void setColor(String newColor){
-        color = newColor;
+        this.color = newColor;
     }
 
-    void setTip(int newTip){
-        tip = newTip;
-    }
-}
-
-class Student {
-    String name;
-    int age;
-    float percentage;
-
-    void calcPercentage(int phy, int chem, int math){
-        percentage = (phy + chem + math) / 3;
+    void setTip(int tip){
+        this.tip = tip;
     }
 }
+
 
