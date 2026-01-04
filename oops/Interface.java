fc4383de6e3b9eafd99bb8eb940b951c0c4c2865
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-01-04 11:42:33 +0530

    Add Java interface example to demonstrate abstraction

---
+++ oops/Interface.java
@@ -0,0 +1,30 @@
+package oops;
+
+interface ChessPlayer {
+    void moves();
+}
+
+class Queen implements ChessPlayer {
+    public void moves(){
+        System.out.println("up, down, left, right,diagonal (in all direction)");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right");
+    }
+}
+
+class King implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right, diagonal - (by 1 step)");
+    }
+}
+
+public class Interface {
+    public static void main(String args[]){
+        Queen q = new Queen();
+        q.moves();
+    }
+}
